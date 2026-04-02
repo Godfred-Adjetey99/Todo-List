f608ebb9bf1851fc0c97d637b5b288fe29f91098
Godfred Adjetey <godfredadjetey99@gmail.com>
2026-04-02 14:54:55 +0000

    added a js to create a new task and delete button

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ const todoInput = document.getElementById("todoInput");
 const addTodoButton = document.getElementById("addTodoButton");
 const todoList = document.getElementById("todoList");
 
-const todos = [];
+const deleteTask = (button) => {
+  button.parentElement.remove();
+};
 
-const addTodo = () => {
-  const todoText = todoInput.value.trim();
-  if (todoText) {
-    const todo = {
-      id: Date.now(),
-      text: todoText,
-    };
-    todos.push(todo);
-    todoInput.value = "";
-    renderTodos();
-  }
+const addTask = () => {
+  todoInput.value
+    ? (todoList.innerHTML += `<li>${todoInput.value} <button onclick="deleteTask(this)">Delete</button></li>`)
+    : alert("Please enter a task.");
+  todoInput.value = "";
 };
