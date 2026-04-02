703682ca2462387e45069bedc437fa6b596ceff5
Godfred Adjetey <godfredadjetey99@gmail.com>
2026-04-02 16:40:51 +0000

    addad an array to store the todo list

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const todoInput = document.getElementById("todoInput");
 const addTodoButton = document.getElementById("addTodoButton");
 const todoList = document.getElementById("todoList");
 
+const toDoItems = [];
+
 const deleteTask = (button) => {
   button.parentElement.remove();
 };
 
 const addTask = () => {
-  todoInput.value
-    ? (todoList.innerHTML += `<li>${todoInput.value} <button onclick="deleteTask(this)">Delete</button></li>`)
-    : alert("Please enter a task.");
+  const item = todoInput.value;
+  toDoItems.push(item);
+
+  console.log(toDoItems);
+
   todoInput.value = "";
 };
