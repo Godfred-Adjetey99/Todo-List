aaaf2d263e5ae7fc8ffc7b6f1db2344310316b4b
Godfred Adjetey <godfredadjetey99@gmail.com>
2026-04-04 08:49:25 +0000

    added duedate to the todo list

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-const todoInput = document.getElementById("todoInput");
-const addTodoButton = document.getElementById("addTodoButton");
-const todoList = document.getElementById("todoList");
+const todoInput = document.querySelector("#todoInput");
+const dateInput = document.querySelector("#dueDateInput");
+const addTodoBtn = document.querySelector("#addTodoButton");
 
-const toDoItems = [];
+const todoList = [{ name: "wash cloths", dueDate: "22-040-26" }];
 
-const deleteTask = (button) => {
-  button.parentElement.remove();
+const renderHtml = () => {
+  let todoHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const addListObject = todoList[i];
+    const { name, dueDate } = addListObject;
+
+    const html = `
+    <p>
+    ${name} - ${dueDate}
+    <button onClick="todoList.splice(${i}, 1)
+    renderHtml()
+    ">Delete</button>
+    </p>`;
+    todoHTML += html;
+  }
+
+  document.querySelector("#todoHTML").innerHTML = todoHTML;
 };
 
-const addTask = () => {
-  const item = todoInput.value;
-  toDoItems.push(item);
+renderHtml();
 
-  console.log(toDoItems);
+const addTask = () => {
+  const name = todoInput.value;
+  const dueDate = dateInput.value;
+  todoList.push({ name, dueDate });
 
   todoInput.value = "";
+
+  renderHtml();
 };
